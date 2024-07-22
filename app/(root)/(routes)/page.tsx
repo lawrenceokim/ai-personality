e2c590e30b19cbadfc75b94da683e075f22023d1
lawrenceokim <lawrence4okim@gmail.com>
2024-07-22 01:25:32 +0100

    initial commit, setups done

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function RootPage() {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
+
+export default RootPage;
